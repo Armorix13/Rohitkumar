29a5061cd68150382b25a5bcfe3e402c0694b6ff
IzRohitoP <rohitreso13@gmail.com>
2024-09-14 22:49:04 +0530

    second  commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "161078a38ebceb16"; }
+/******/ 		__webpack_require__.h = function() { return "0807506ee191c58e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
